56717cceaf22dcd74458d150e565193799d839c3
Lalith Kumar Bhudeti <lalithkumarbhudeti@Laliths-MacBook-Pro.local>
2025-08-04 21:50:47 +0530

    Update project files

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Header.css";
+// import './index.css'
+import { BiMenuAltRight } from "react-icons/bi";
+import OutsideClickHandler from "react-outside-click-handler";
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const getMenuStyles = (menuOpened) => {
+    if (window.innerWidth <= 800) {
+      return { right: menuOpened ? "0" : "-100%" };
+    }
+    return {}; // prevent undefined
+  };
+
   return (
     <section className="H-wrapper">
-        <div className="H-container">
+      <div className="flexCenter paddings innerWidth H-container">
+        <img src="./logo.png" alt="logo" width={100} />
 
-            <img src="./logo.png" alt="logo" width={100} />
-            <div className="H-menu">
-                <a href="">Residencies</a>
-                <a href="">Our Values</a>
-                <a href="">Contact Us</a>
-                <a href="">Get Started</a>
-                
-                <button>
-                    <a href="">Contact</a>
-                </button>
-                
-            </div>
+        <OutsideClickHandler onOutsideClick={() => {setMenuOpened(false)}}>
+          <div className="flexCenter H-menu" style={getMenuStyles(menuOpened)}>
+            <a href="">Residencies</a>
+            <a href="">Our Values</a>
+            <a href="">Contact Us</a>
+            <a href="">Get Started</a>
+
+            <button className="button">
+              <a href="">Contact</a>
+            </button>
+          </div>
+        </OutsideClickHandler>
+
+        <div
+          className="menu-icon"
+          onClick={() => setMenuOpened((prev) => !prev)}
+        >
+          <BiMenuAltRight size={30} />
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-
-
-export default Header
+export default Header;
+ 
